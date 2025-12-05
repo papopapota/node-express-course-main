3c94f7ce93ba0d0db59960fd2ad1357fe209e58d
Daniel Olivares <daniel_olivares09@hotmail.com>
2025-12-05 17:19:51 -0500

    uso del jwt para codificar payload

--- node-express-course-main/05-JWT-Basics/starter/controllers/main.js
+++ node-express-course-main/05-JWT-Basics/starter/controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
@@ -5,18 +6,32 @@ const login = async (req, res) => {
     if (!username || !password) {
         throw new CustomAPIError('Please provide email and password', 400);
     }
-    //mongoose validation will also work
-    //joi
-    //yup
-    res.send('Login Successful');
+    //DEMO
+    const id = new Date().getDate();
+    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+    res.status(200).json({ msg: 'user created', token });
 };
 
 const dashboard = async (req, res) => {
-    const luckuNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({
-        msg: `Hello, Jhon Doe`,
-        secret: `Heres is your authorized data, your lucky numbers is ${luckuNumber}`
-    })  
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomAPIError('Invalid authorization header', 401);
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        //console.log(decoded);
+        const luckuNumber = Math.floor(Math.random() * 100);
+        res.status(200).json({
+            msg: `Hello, ${decoded.username}`,
+            secret: `Heres is your          q authorized data, your lucky numbers is ${luckuNumber}`
+        })
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this route', 401);
+    }
+
 }
 
 module.exports = {
