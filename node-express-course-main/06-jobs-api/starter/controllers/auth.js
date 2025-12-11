578694d1f187c0d066905733420935a4218947ab
Daniel Olivares <76667767+papopapota@users.noreply.github.com>
2025-12-11 18:48:41 -0500

    se jobs api

---
+++ node-express-course-main/06-jobs-api/starter/controllers/auth.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+const jwt = require('jsonwebtoken');
+
+const register = async (req, res) => {
+    // const { name, email, password } = req.body;
+    // if (!name || !email || !password) {
+    //     throw new BadRequestError('Please provide name, email and password');
+    // }
+    const user = await User.create({ ...req.body });
+    const token = jwt.sign({userId:user._id,name:user.name},'jwtScret',{expiresIn:'30d'});
+    res.status(StatusCodes.CREATED).json({ user:{name:user.getName()} ,token });
+};
+
+const login = async (req, res) => {
+    res.send('login user');
+};
+
+module.exports = {
+    register,
+    login,
+};
---
+++ node-express-course-main/06-jobs-api/starter/controllers/jobs.js
@@ -0,0 +1,28 @@
+
+const getAllJobs = (req, res) => {
+    res.send('get all jobs');
+};
+
+const getJob = (req, res) => {
+    res.send('get job');
+};
+
+const createJob = (req, res) => {
+    res.send('create job');
+};
+
+const updateJob = (req, res) => {
+    res.send('update job');
+};
+
+const deleteJob = (req, res) => {
+    res.send('delete job');
+};
+
+module.exports = {
+    getAllJobs,
+    getJob,
+    createJob,
+    updateJob,
+    deleteJob,
+};
