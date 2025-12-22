2b541dccbbb7571d0c869cdd6a0da5bce3b0235d
Daniel Olivares <76667767+papopapota@users.noreply.github.com>
2025-12-22 16:59:47 -0500

    se agrego swagger, se mejoro la authentication y se agrego seguridad a app.js

--- node-express-course-main/06-jobs-api/starter/controllers/auth.js
+++ node-express-course-main/06-jobs-api/starter/controllers/auth.js
@@ -14,9 +14,9 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
     const { email, password } = req.body;
-    // if (!email || !password) {
-    //     throw new BadRequestError('Please provide email and password');
-    // }
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password');
+    }
     const user = await User.findOne({ email });
     if (!user) {
         throw new UnauthenticatedError('Invalid Credentials.');
